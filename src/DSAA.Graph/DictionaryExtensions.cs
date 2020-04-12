5aa26c01677560a88a6f7afd941732a00da8543e
Auris G <aurimas.gecas@gmail.com>
2020-04-12 12:24:20 +0100

    #1674 - Added extension methods to allow easy Graph building from Dictionaries.

---
+++ src/DSAA.Graph/DictionaryExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using DSAA.Graph.Fluent;
+
+namespace DSAA.Graph
+{
+    public static class DictionaryExtensions
+    {
+        public static IGraph<T> ToGraph<T, TValues>(this IDictionary<T, TValues> lookup, Func<ISetGraphDirection<T>, IBuildGraph<T>> builder)
+        where TValues : IEnumerable<T>
+        {
+            if (lookup == null) throw new ArgumentNullException(nameof(lookup));
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            var graph = builder(new GraphBuilder<T>()).Build();
+
+            foreach (var pair in lookup)
+            {
+                graph.AddVertex(pair.Key);
+
+                foreach (var value in pair.Value)
+                {
+                    graph.AddEdge(pair.Key, value);
+                }
+            }
+
+            return graph;
+        }
+    }
+}
